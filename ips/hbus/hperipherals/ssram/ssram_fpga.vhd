8afd89f03841233f752014e4aecae18230acedc7
Luca-Dalmasso <luca.talpax@gmail.com>
2022-05-26 16:08:21 +0000

    refactor: sram has been updated to be compatible with CycloneV embedded memory blocs
    both read and write processes are synchronous, read process is now Synchronous on falling edges so that stalls are not required

--- ips/hbus/hperipherals/ssram/ssram_fpga.vhd
+++ ips/hbus/hperipherals/ssram/ssram_fpga.vhd
@@ -40,6 +40,8 @@ LIBRARY IEEE;
 LIBRARY STD;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
+USE IEEE.STD_LOGIC_TEXTIO.ALL;
+USE STD.TEXTIO.ALL;
 
 ENTITY ssram_fpga IS
 	GENERIC (
@@ -58,14 +60,48 @@ END ssram_fpga;
 
 --Behavior:
 --Synchronous write
---Asynchronous read
---(also possible to make both processes Synchronous)
+--Synchronous read
+--(both process needs to be Synchronous otherwise the memory is not synthesized in the fpga memories)
 
-ARCHITECTURE behavior OF ssram_fpga IS
+ARCHITECTURE behavior OF ssram_fpga IS 
 
-    TYPE mem_type IS ARRAY (0 TO 2**addressWidth - 1) OF STD_LOGIC_VECTOR (dataWidth-1 DOWNTO 0);
-	SIGNAL mem : MEM_TYPE;
-	SIGNAL dOut: STD_LOGIC_VECTOR (dataWidth - 1 DOWNTO 0);   
+    TYPE MEM_TYPE IS ARRAY (0 TO 2**addressWidth - 1) OF STD_LOGIC_VECTOR (dataWidth-1 DOWNTO 0);
+	
+	FUNCTION init_my_ram (filename : string) RETURN MEM_TYPE IS
+    FILE f : TEXT;
+    VARIABLE m : MEM_TYPE;
+    VARIABLE adr: STD_LOGIC_VECTOR(addressWidth-1 DOWNTO 0);
+	VARIABLE memline: LINE;
+	VARIABLE linechar: CHARACTER;
+	VARIABLE read_address: STD_LOGIC_VECTOR (31 DOWNTO 0);
+	VARIABLE read_data: STD_LOGIC_VECTOR (31 DOWNTO 0);
+	VARIABLE index: INTEGER:=0;
+	VARIABLE end_iram : INTEGER := 16#FFFFF#;
+    BEGIN
+        file_open(f, filename, read_mode);
+        for index in MEM_TYPE'range loop
+            IF ENDFILE(f) THEN
+                exit;
+            END IF;
+	        READLINE (f, memline);
+			HREAD (memline, read_address);
+			READ (memline, linechar); -- read character '_' 
+		    HREAD (memline, read_data);
+		    IF UNSIGNED(read_address) > end_iram THEN -- it is a data address (see file link.common.ld)
+			    adr := '1' & read_address(addressWidth-2 DOWNTO 0);
+			ELSE -- it is a program address
+				adr := '0' & read_address(addressWidth-2 DOWNTO 0);
+			END IF;
+				m(TO_INTEGER(UNSIGNED(adr))) 	 := read_data(7 DOWNTO 0);
+				m(TO_INTEGER(UNSIGNED(adr) + 1)) := read_data(15 DOWNTO 8);
+				m(TO_INTEGER(UNSIGNED(adr) + 2)) := read_data(23 DOWNTO 16);
+				m(TO_INTEGER(UNSIGNED(adr) + 3)) := read_data(31 DOWNTO 24);
+		END LOOP;
+		FILE_CLOSE (f);
+        RETURN m;
+    END init_my_ram;
+   
+	SIGNAL mem : MEM_TYPE:=init_my_ram("./slm_files/spi_stim.txt"); 
 
 BEGIN
 
@@ -76,18 +112,15 @@ BEGIN
             mem(TO_INTEGER(UNSIGNED(address))) <= dataIn;
         END IF;
     END PROCESS;
-    
-    --Asynch read with enable
-    PROCESS(readMem, address)
+      
+    --Synch read with enable
+    PROCESS(clk)
     BEGIN
-        IF readMem='1' then
-			dOut <= mem(TO_INTEGER(UNSIGNED(address)));
+        IF (falling_edge(clk) and readMem='1') THEN
+            dataOut <= mem(TO_INTEGER(UNSIGNED(address)));
         END IF;
     END PROCESS;
-    
-    dataOut<=dOut;
 
 END behavior;
-
 
 
