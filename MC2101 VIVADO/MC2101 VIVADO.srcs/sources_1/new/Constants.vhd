6ca29f8df5d83ce96245a5a418065c966c01c9a9
juanjse13 <restrepotorojuanjose@gmail.com>
2023-12-05 10:22:53 +0100

    NEW: New file that includes all the respective constants

---
+++ MC2101 VIVADO/MC2101 VIVADO.srcs/sources_1/new/Constants.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12/05/2023 09:14:02 AM
+-- Design Name: 
+-- Module Name: Constants - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+--This file contains the constant values related to both AFTAB and MC2101
+----------------------------------------------------------------------------------
+
+LIBRARY IEEE; 
+USE IEEE.STD_LOGIC_1164.ALL;
+
+PACKAGE Constants IS
+    --Constants related to AFTAB memory
+    
+    CONSTANT dataWidth      : INTEGER := 8;
+	CONSTANT addressWidth   : INTEGER := 32;
+	CONSTANT actual_address : INTEGER := 13;
+	CONSTANT size           : INTEGER := 2 ** actual_address; -- 2^12 for data and 2^12 for instr, 4 K each  
+    -- Memory boundaries - change this according to the linker script: sw/ref/link.common.ld
+	CONSTANT base_iram : INTEGER := 16#00000#; 
+	CONSTANT end_iram : INTEGER := 16#FFFFF#;
+
+	CONSTANT base_dram : INTEGER := 16#100000#; 
+	CONSTANT end_dram : INTEGER := 16#100600#;
+
+	CONSTANT base_dram_actual : INTEGER := 16#1000#;
+	CONSTANT size_dram : INTEGER := 16#0FFF#;
+	
+	--Constants related to MC2101
+    CONSTANT dataWidthSRAM      : INTEGER :=8;
+	CONSTANT addressWidthSRAM   : INTEGER :=13;
+
+END Constants;
+
