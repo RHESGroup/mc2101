d01840d529dc493d56756371e40c1b23dba203b1
Juan Jose Restrepo <restrepotorojuanjose@gmail.com>
2024-10-16 16:23:34 +0200

    FIX: change paths for saving the IPs

---
+++ target/xilinx/IP/ILA/run.tcl
@@ -0,0 +1,18 @@
+set partNumber $::env(XILINX_PART)
+set boardName  $::env(XILINX_BOARD)
+
+set ipName ila_0
+
+set current_directory [file normalize [pwd]]
+puts $current_directory
+
+create_project $ipName $current_directory/IP/ILA -force -part $partNumber
+set_property board_part $boardName [current_project]
+set_property target_language VHDL [current_project]
+
+create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name $ipName
+
+close_project
+
+
+
