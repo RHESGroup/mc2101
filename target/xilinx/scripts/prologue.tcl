8a5e2d58366d4ab932f3b991448ee697b533c617
Juan Jose Restrepo <restrepotorojuanjose@gmail.com>
2024-11-09 11:43:48 +0100

    FIX: set VHDL as the target language

--- target/xilinx/scripts/prologue.tcl
+++ target/xilinx/scripts/prologue.tcl
@@ -4,6 +4,7 @@ set project $::env(PROJECT)
 
 create_project $project ./Work_directory -force -part $::env(XILINX_PART)
 set_property board_part $::env(XILINX_BOARD) [current_project]
+set_property target_language VHDL [current_project]
 
 # set number of threads to 8 (maximum, unfortunately)
 set_param general.maxThreads 8
